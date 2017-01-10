c241d973a0cb11fb68787fad0dc562c58b7f176a
Kevin Angstadt <kaa2nx@virginia.edu>
2017-01-10 16:46:40 -0500

    Adding C++ Testing

---
+++ C++/tests/readwrite.cpp
@@ -0,0 +1,16 @@
+/*
+ * test MNRL API by reading in MNRL file and writing it back out
+ */
+#include <iostream>
+#include "mnrl.hpp"
+using namespace std;
+int main(int argc, char *argv[]) {
+	if(argc != 3) {
+		cout << "usage: readwrite infile outfile" << endl;
+		return 1;
+	}
+
+	shared_ptr<MNRL::MNRLNetwork> m = MNRL::loadMNRL(string(argv[1]));
+	m->exportToFile(string(argv[2]));
+	return 0;
+}
