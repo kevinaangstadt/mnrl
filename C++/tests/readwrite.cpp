781dc91fa96a8afe97f0d963c04b03c9a1ede428
Kevin Angstadt <angstadt@umich.edu>
2019-06-05 17:10:58 -0400

    Improved testing

--- C++/tests/readwrite.cpp
+++ C++/tests/readwrite.cpp
@@ -5,12 +5,12 @@
 #include "mnrl.hpp"
 using namespace std;
 int main(int argc, char *argv[]) {
-	if(argc != 3) {
-		cout << "usage: readwrite infile outfile" << endl;
+	if(argc < 3 || argc > 4) {
+		cout << "usage: readwrite infile outfile pretty|condensed" << endl;
 		return 1;
 	}
 
 	MNRL::MNRLNetwork m = MNRL::loadMNRL(string(argv[1]));
-	m.exportToFile(string(argv[2]));
+	m.exportToFile(string(argv[2]), argc == 4 ? argv[3][0] == 'p' : false);
 	return 0;
 }
